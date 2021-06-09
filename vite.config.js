cec1d90d91afcfb31e2784f24f84dd2a85c4cead
Jack Guinane <jackguinane@gmail.com>
2021-06-09 00:22:00 -0700

    update webmanifest

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,29 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-	  vue(),
-	  VitePWA()
+	plugins: [
+		vue(),
+		VitePWA({
+			manifest: {
+				"name": "Typeright",
+				"short_name": "Typeright",
+				"icons": [
+					{
+						"src": "/android-chrome-192x192.png",
+						"sizes": "192x192",
+						"type": "image/png"
+					},
+					{
+						"src": "/android-chrome-512x512.png",
+						"sizes": "512x512",
+						"type": "image/png"
+					}
+				],
+				"theme_color": "#efefef",
+				"background_color": "#efefef",
+				"display": "standalone",
+				"start_url": "/"
+			}
+		})
 	]
 })
