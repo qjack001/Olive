87863295632f768fa28cf135b4a9ac02df7ee436
Jack Guinane <jackguinane@gmail.com>
2023-04-03 19:06:27 -0700

    Create app components

---
+++ src/renderer/components/Panel.vue
@@ -0,0 +1,29 @@
+<template>
+	<div id="panel"/>
+</template>
+
+<style scoped>
+	div
+	{
+		z-index: 999;
+		pointer-events: none;
+
+		background: var(--tint);
+		backdrop-filter: blur(5px);
+
+		position: fixed;
+		top: calc(50vh + 23px);
+		left: 0;
+		right: 0;
+		bottom: 0;
+	}
+
+	@media print
+	{
+		div
+		{
+			opacity: 0;
+			display: none;
+		}
+	}
+</style>
