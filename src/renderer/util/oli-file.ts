e9bd7bdef150211772887811c548e0d5a1362b13
Jack Guinane <jackguinane@gmail.com>
2023-05-07 22:34:35 -0700

    Create compressed OliFile

--- src/renderer/util/oli-file.ts
+++ src/renderer/util/oli-file.ts
@@ -1,4 +1,5 @@
 import { ColorName } from './paper-color'
+import { v4 as uuid } from 'uuid'
 
 /**
  * Character object represents a single character printed on the page.
@@ -41,3 +42,70 @@ export interface OliFileVersion1
 }
 
 export type OliFile = OliFileVersion1
+type CompressedText = [number, number, string, boolean] | [number, number, string]
+type CompressedPen = [number, number, number, number] | null
+
+export interface CompressedOliFile
+{
+	version: 2.0
+	text?: CompressedText[]
+	pen?: CompressedPen[]
+	color?: ColorName
+}
+
+
+export function compress(file: OliFile): CompressedOliFile
+{
+	const version = 2.0
+	const color = file.paperColor
+
+	const text: CompressedText[] | undefined = file.content?.map((char) => {
+		return (char.erase)
+			? [char.posX, char.posY, char.value, char.erase]
+			: [char.posX, char.posY, char.value]
+	})
+
+	const pen: CompressedPen[] | undefined = file.penMarkings?.map((point) => {
+		return (point == null)
+			? null
+			: [round(point.x), round(point.y), round(point.w), point.o]
+	})
+
+	return { version, text, pen, color }
+}
+
+export function uncompress(file: CompressedOliFile): OliFile
+{
+	const version = 1.0
+	const paperColor = file.color
+
+	const content: Character[] | undefined = file.text?.map((char, index) => {
+		return {
+			posX: char[0],
+			posY: char[1],
+			value: char[2],
+			erase: char[3] ?? false,
+			id: uuid(),
+		}
+	})
+
+	const penMarkings: Point[] | undefined = file.pen?.map((point) => {
+		if (point == null) {
+			return null
+		}
+		
+		return {
+			x: point[0],
+			y: point[1],
+			w: point[2],
+			o: point[3],
+		}
+	})
+
+	return { version, content, penMarkings, paperColor }
+}
+
+function round(num: number): number
+{
+	return Math.round(num * 100) / 100
+}
